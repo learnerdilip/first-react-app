21b5b7a0db0e1cc1b4728fe3e502b13343625b98
learnerdilip <diliptalks@gmail.com>
2020-01-20 12:21:12 +0100

    the reader page

---
+++ src/Title.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+export default class Title extends Component {
+  render() {x
+    return <h1>Hi there from the first React App! {this.props.content}</h1>;
+  }
+}
